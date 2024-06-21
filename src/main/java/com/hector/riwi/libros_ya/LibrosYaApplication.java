3f5836bb0abc1c3a3cba9bb5a1cd870a8f402818
ralexale <ralexale@gmail.com>
2024-06-21 06:37:45 -0500

    init commit

---
+++ src/main/java/com/hector/riwi/libros_ya/LibrosYaApplication.java
@@ -0,0 +1,13 @@
+package com.hector.riwi.libros_ya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrosYaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrosYaApplication.class, args);
+	}
+
+}
